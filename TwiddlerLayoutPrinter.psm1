584938eb2ecd82d9527565fe036cd346312ec41f
Geoff-Lillis <geoff.lillis@gmail.com>
2021-05-20 05:08:37 +0100

    Initial commit

---
+++ TwiddlerLayoutPrinter.psm1
@@ -0,0 +1,256 @@
+﻿function Get-SpecialKeys {
+    @{'<Backspace>'                           = "←X"
+        '<Left Ctrl><Backspace></Left Ctrl>'  = "←←X"
+        '<UpArrow>'                           = "↑"
+        '<PageUp>'                            = "PG↑"
+        '<RightArrow>'                        = "→"
+        '<DownArrow>'                         = "↓"
+        '<PageDown>'                          = "PG↓"
+        '<LeftArrow>'                         = "←"
+        '<Left Ctrl><UpArrow></Left Ctrl>'    = "↑↑"
+        '<Left Ctrl><RightArrow></Left Ctrl>' = "→→"
+        '<Left Ctrl><DownArrow></Left Ctrl>'  = "↓↓"
+        '<Left Ctrl><LeftArrow></Left Ctrl>'  = "←←"
+        '<Return>'                            = "←|"
+        '<PrintScreen>'                       = "PSC"
+        '<Delete>'                            = "DEL"
+        '<Escape>'                            = "ESC"
+        '<Home>'                              = "HME"
+        '<End>'                               = "END"
+        '<F1>'                                = "F1"
+        '<F10>'                               = "F10"
+        '<F11>'                               = "F11"
+        '<F12>'                               = "F12"
+        '<F2>'                                = "F2"
+        '<F3>'                                = "F3"
+        '<F4>'                                = "F4"
+        '<F5>'                                = "F5"
+        '<F6>'                                = "F6"
+        '<F7>'                                = "F7"
+        '<F8>'                                = "F8"
+        '<F9>'                                = "F9"
+        '<Tab>'                               = "TAB"
+        '<CapsLock>'                          = "CAP"
+        '<NumLock>'                           = "NUM"
+        ' '                                   = '_'
+    }
+}
+
+function Get-AllPossibleChords {
+    $Keys = "OLMR".ToCharArray()
+    for ($i = 0; $i -lt $Keys.Length; $i++) {
+        for ($j = 0; $j -lt $Keys.Length; $j++) {
+            for ($k = 0; $k -lt $Keys.Length; $k++) {
+                for ($l = 0; $l -lt $Keys.Length; $l++) {
+                    $Keys[$l] + $Keys[$k] + $Keys[$j] + $Keys[$i]
+                }
+            }
+        }    
+    }
+}
+
+function Get-LayoutFromCSV {
+    Param (
+        #Test it exists, is valid CSV
+        $Path
+    )
+    $SpecialKeys = Get-SpecialKeys
+    $NoModifierKeysPrefix = "   O "
+    Import-Csv -Path $Path | Where-Object { $_.Chord.StartsWith($NoModifierKeysPrefix) } |
+        ForEach-Object {
+            $Chord = $_.Chord -Replace $NoModifierKeysPrefix, ""
+            [PSCustomObject]@{
+                Chord      = $Chord
+                KeyStrokes = $_.'Key Strokes'
+                KeysCount  = ($Chord -replace "O", "").length
+                Type       = Get-KeystrokeType $_.'Key Strokes' $SpecialKeys
+            }
+        }
+}
+
+function Get-KeystrokeType($Keystrokes, $SpecialKeys) {
+    if (-not $Keystrokes) {
+        "Unassigned"
+    } elseif ($SpecialKeys.Contains($Keystrokes)) {
+        "Special"
+    } elseif ($Keystrokes.length -gt 1) {
+        "MultiCharacter"
+    } elseif ($Keystrokes -match "^[0-9]$") {
+        "Number"
+    } else {
+        "Letter"
+    }
+}
+
+function Get-ForegroundColorFromType ($Type) {
+    switch ($Type) {
+        "Unassigned" { "Magenta" }
+        "Special" { "Cyan" }
+        "MultiCharacter" { "Green" }
+        "Number" { "Yellow" }
+        "Letter" { "White" }
+    }
+}
+
+function Get-RowWithOneKeyPressed ($Key) {
+    switch ($Key) {
+        "L" { " *          " }
+        "M" { "     *      " }
+        "R" { "         *  " }
+    }
+}
+    
+function Get-DisplayedKeystrokes($Keystrokes, $Type, $SpecialKeys) {
+    $Return = switch ($Type) {
+        "Unassigned" { "[]" }
+        "Special" { $SpecialKeys[$Keystrokes] }
+        Default { $Keystrokes }
+    }
+    $Return.PadLeft(2, " ").PadRight(4, " ")
+}
+
+function Get-CellContents ($ChordToKeystrokes, $PartialChord) {
+    #Writing this in an attempt to remove duplicates
+    #Get a better understanding of what's going on here.
+    #It's not a grid. Cell? 
+    foreach ($Row in 0..3) {
+        $KeyAtRowToDisplay = $PartialChord.Chars($Row)
+        if ($KeyAtRowToDisplay -eq "O") {
+            $Start = $PartialChord.Substring(0, $Row)
+            $End = $PartialChord.Substring($Row + 1)
+            foreach ($Button in "LMR".ToCharArray()) {
+                $Chord = "$Start$Button$End"
+                if ($ChordToKeystrokes.ContainsKey($Chord)) {
+                    $ChordToKeystrokes[$Chord]
+                }
+            }
+        }
+    }
+}
+
+function Get-ChordsWithoutDuplicatedKeystrokes ($ChordToKeystrokes) {
+    #Make this clearer. Get-ReducedChordsThatStillPrintAllKeystrokes?
+    $Chords = Get-AllPossibleChords
+    foreach ($Chord in $Chords) {
+        $Keystrokes = Get-CellContents $ChordToKeystrokes $Chord
+        if ($null -eq $AlreadyPrinted) {
+            $NovelKeystrokes = $Keystrokes
+        } elseif ($Keystrokes) {
+            $NovelKeystrokes = Compare-Object $AlreadyPrinted $Keystrokes | 
+                Where-Object SideIndicator -EQ "=>"
+        } else {
+            $NovelKeystrokes = $null
+        }
+        if ($NovelKeystrokes.count -gt 0) {
+            $Chord
+            $AlreadyPrinted += $Keystrokes
+        }
+    }
+}
+
+function Write-FormattedRowContents {
+    #You can't call it "Get" if it doesn't return anything and it changes state.
+    #Write-FormattedRowContents?
+    #Split out a "Write-RowWithPressedButton" and "Write-RowWithNoPressedButtons"
+    Param (
+        [ValidateRange(0,3)]
+        [int]$Row,
+        [Hashtable]
+        $ChordToKeystrokes,
+        [ValidatePattern('^(O|L|M|R){4}$')]
+        $PartialChord
+    )
+    $KeyAtRowToDisplay = $PartialChord.Chars($Row)
+    if ($KeyAtRowToDisplay -match "(L|M|R)") {
+        Write-Host (Get-RowWithOneKeyPressed $KeyAtRowToDisplay) -NoNewline -ForegroundColor Red
+    } else {
+        $SpecialKeys = Get-SpecialKeys
+        $Start = $PartialChord.Substring(0, $Row)
+        $End = $PartialChord.Substring($Row + 1)
+        foreach ($Button in "LMR".ToCharArray()) {
+            $Chord = "$Start$Button$End"
+            $Keystrokes = $ChordToKeystrokes[$Chord]
+            $Type = Get-KeystrokeType $Keystrokes $SpecialKeys
+            $ForegroundColor = Get-ForegroundColorFromType $Type
+            $DisplayedKeystrokes = Get-DisplayedKeystrokes $Keystrokes $Type $SpecialKeys
+            Write-Host $DisplayedKeystrokes -ForegroundColor $ForegroundColor -NoNewline
+        }
+    }
+}
+
+function Get-ChordToKeystrokesHashtable ($Layout) {
+    $ChordToKeystrokes = @{}
+    $Layout | ForEach-Object {
+        $ChordToKeystrokes.Add($_.Chord, $_.KeyStrokes)
+    }
+    $ChordToKeystrokes
+}
+
+function Write-TopOfGrids ([int]$GridsToDisplay) {
+    for ($i = 0; $i -lt $GridsToDisplay; $i++) {
+        Write-Host "┌─────────────┐" -NoNewline
+    }
+    Write-Host
+}
+
+function Write-BottomOfGrids ([int]$GridsToDisplay) {
+    for ($i = 0; $i -lt $GridsToDisplay; $i++) {
+        Write-Host "└─────────────┘" -NoNewline
+    }
+    Write-Host
+}
+
+function Write-ChordName ([int]$GridsToDisplay, $ChordsToDisplay, $GridIndex) {
+    for ($i = 0; $i -lt $GridsToDisplay; $i++) {
+        Write-Host "│" -NoNewline
+        Write-Host "    $($ChordsToDisplay[$GridIndex+$i])     " -NoNewline -ForegroundColor DarkGray
+        Write-Host "│" -NoNewline 
+    }
+    Write-Host
+}
+
+function Write-FormattedRow ([int]$GridsToDisplay, $ChordsToDisplay, $ChordToKeystrokes, $GridIndex, $RowIndex) {
+    for ($i = 0; $i -lt $GridsToDisplay; $i++) {
+        Write-Host "│ " -NoNewline
+        (Write-FormattedRowContents $RowIndex $ChordToKeystrokes $ChordsToDisplay[$GridIndex + $i])
+        Write-Host "│" -NoNewline
+    }
+    Write-Host
+}
+
+function Format-Layout {
+    Param(
+        [Parameter(Mandatory)]
+        $Layout, 
+        $GridsPerRow = [math]::Floor($Host.UI.RawUI.WindowSize.Width / 15),
+        [ValidateRange(0, 3)] 
+        $MinUnusedKeys = 1, #Is this compatible with ReduceDuplicates?
+        [switch]$ChordHeader,
+        [switch]$ReduceDuplicates
+    )
+    $ChordToKeystrokes = Get-ChordToKeystrokesHashtable $Layout
+    $ChordsToDisplay = if ($ReduceDuplicates) {
+        Get-ChordsWithoutDuplicatedKeystrokes $ChordToKeystrokes
+    } else {
+        Get-AllPossibleChords | Where-Object { ([regex]::Matches($_, "O")).Count -ge $MinUnusedKeys }
+    }
+    for ($j = 0; $j -lt $ChordsToDisplay.Length; $j += $GridsPerRow) {
+        $RemainingChordCount = ($ChordsToDisplay.Count - $j)
+        $GridsToDisplayCount = [math]::min($RemainingChordCount, $GridsPerRow)
+        Write-TopOfGrids $GridsToDisplayCount
+        if ($ChordHeader) {
+            Write-ChordName $GridsToDisplayCount -GridIndex $j -ChordsToDisplay $ChordsToDisplay 
+        }
+        for ($i = 0; $i -lt 4; $i++) {
+            $FormattedRowParams = @{
+                GridsToDisplay    = $GridsToDisplayCount
+                ChordsToDisplay   = $ChordsToDisplay
+                ChordToKeystrokes = $ChordToKeystrokes
+                GridIndex         = $j
+                RowIndex          = $i
+            }
+            Write-FormattedRow @FormattedRowParams
+        }
+        Write-BottomOfGrids $GridsToDisplayCount
+    }
+}
---
+++ TwiddlerLayoutPrinter.tests.ps1
@@ -0,0 +1,230 @@
+Import-Module .\TwiddlerLayoutPrinter.psm1
+
+Describe Get-SpecialKeys {
+    It "Should return a hashtable" {
+        Get-SpecialKeys | Should -BeOfType "Hashtable"
+    }
+    It "Should not have any symbols longer than three characters" {
+        (Get-SpecialKeys).values | Where-Object length -gt 3 | Should -BeNullOrEmpty
+    }
+}
+
+Describe Get-AllPossibleChords {
+    It "Should have 256 unique values" {
+        (Get-AllPossibleChords | Get-Unique).Count | Should -Be 256
+    }
+    It "All should match the pattern ^(O|L|M|R){4}$" {
+        Get-AllPossibleChords | Where-Object { $_ -match "^(O|L|M|R){4}$" } | 
+            Measure-Object | Select-Object -ExpandProperty Count | Should -Be 256
+    }
+}
+
+Describe Get-LayoutFromCSV {
+    BeforeAll {
+        @"
+        "Chord","Key Strokes"
+        "   N OOOL","<Left GUI>s</Left GUI>"
+        "   O ORMM","``"
+        "   O OLMM","\"
+        "   O OMMM",";"
+        "   N OROO","<Left GUI>r</Left GUI>"
+        "   O ROOO","o"
+        "   O MOOO","i"
+        "   O LOOO","e"
+        "   O OROO","r"
+        "   O RROO","or"
+"@ | Out-File TestDrive:\Layout.csv
+    }
+    It "Loads chords from a Twiddler tuner export, ignoring chords with modifiers, adding number of keys pressed and type" {
+        $Layout = Get-LayoutFromCSV TestDrive:\Layout.csv
+        $Layout | Should -HaveCount 8
+        $Layout.KeyStrokes | Should -Be "``", "\", ";", "o", "i", "e", "r", "or"
+        $Layout.KeysCount | Should -Be 3, 3, 3, 1, 1, 1, 1, 2
+        $Layout.Type[0..($Layout.Length - 2)] | Select-Object -Unique | Should -Be "Letter"
+        $Layout.Type[-1] | Should -Be "Multicharacter"
+    }
+}
+
+Describe Get-KeystrokeType {
+    BeforeAll {
+        $script:SpecialKeys = Get-SpecialKeys
+    }
+    It "Should return 'Unassigned' when passed a null value" {
+        Get-KeystrokeType -SpecialKeys $SpecialKeys | Should -Be "Unassigned"
+    }    
+    It "Should return 'Special' when passed a special key" {
+        Get-KeystrokeType "<TAB>" -SpecialKeys $SpecialKeys | Should -Be "Special"
+    }    
+    It "Should return 'MultiCharacter' when passed a multicharacter string" {
+        Get-KeystrokeType "the" -SpecialKeys $SpecialKeys | Should -Be "MultiCharacter"
+    }    
+    It "Should return 'Number' in all other cases" {
+        Get-KeystrokeType "1" -SpecialKeys $SpecialKeys | Should -Be "Number"
+    }
+    It "Should return 'Letter' in all other cases" {
+        Get-KeystrokeType "l" -SpecialKeys $SpecialKeys | Should -Be "Letter"
+    }    
+}
+
+Describe Get-ForegroundColorFromType  {
+    $TypesAndColours = @(
+        @{ Type = "Unassigned";     Color = "Magenta" }
+        @{ Type = "Special";        Color = "Cyan"    }
+        @{ Type = "MultiCharacter"; Color = "Green"   }
+        @{ Type = "Number";         Color = "Yellow"  }
+        @{ Type = "Letter";         Color = "White"   }
+    )
+    It "When passed the type '<Type>', it returns the colour <Color>." -ForEach $TypesAndColours {
+        Get-ForegroundColorFromType -Type $Type | Should -Be $Color
+    }
+
+}
+Describe Get-RowWithOneKeyPressed  {
+    $ButtonsAndDisplayedRows = @(
+        @{ Button = "L"; DisplayedRow = " *          " }
+        @{ Button = "M"; DisplayedRow = "     *      " }
+        @{ Button = "R"; DisplayedRow = "         *  " }
+    )
+    It "When '<Button>' is pressed, the displayed row is |$DisplayedRow|" -ForEach $ButtonsAndDisplayedRows {
+        Get-RowWithOneKeyPressed -Key $Button | Should -Be $DisplayedRow
+    }
+}
+Describe Get-DisplayedKeystrokes {
+    BeforeAll {
+        $script:SpecialKeys = Get-SpecialKeys
+    }
+    It "Returns '[]' when the type is 'Unassigned'" {
+        Get-DisplayedKeystrokes -Type "Unassigned" -SpecialKeys $SpecialKeys | Should -Be "[]  "
+    }
+    It "Returns a special display character when the type is 'Special'" {
+        Get-DisplayedKeystrokes -Type "Special" -Keystrokes "<Escape>" -SpecialKeys $SpecialKeys | Should -Be "ESC "
+    }
+    It "Returns the keystrokes when the type is 'Default'" {
+        Get-DisplayedKeystrokes -Type "Default" -Keystrokes "a" -SpecialKeys $SpecialKeys | Should -Be " a  "
+    }
+    It "Uses appropriate padding when the keystrokes contain two characters" {
+        Get-DisplayedKeystrokes -Type "Default" -Keystrokes "aa" -SpecialKeys $SpecialKeys | Should -Be "aa  "
+    }
+}
+Describe Get-CellContents  {
+
+}
+Describe Get-ChordsWithoutDuplicatedKeystrokes  {
+
+}
+Describe Write-FormattedRowContents  {
+    It "Calls Get-RowWithOneKeyPressed if a key is pressed" {
+        Mock Get-RowWithOneKeyPressed -ModuleName TwiddlerLayoutPrinter {}
+        Write-FormattedRowContents -Row 0 -PartialChord "LOOO" -ChordToKeyStrokes @{} |
+            Should -Invoke Get-RowWithOneKeyPressed -ModuleName TwiddlerLayoutPrinter -Exactly 1
+    }
+    It "Prints the keystrokes for buttons on row 1 when the L button of row 0 is pressed" {
+        $ChordToKeystrokes = @{"LROO" = "er"; "LMOO" = "me"; "LLOO" = "he"}
+        Mock Write-Host -ModuleName TwiddlerLayoutPrinter {}
+        Write-FormattedRowContents -Row 1 -PartialChord "LOOO" -ChordToKeyStrokes $ChordToKeystrokes | 
+            Should -Invoke Write-Host -Times 3 -Exactly -ModuleName TwiddlerLayoutPrinter -ParameterFilter {
+                $NoNewLine -and $ForegroundColor -eq "Green"
+            }
+    }
+}
+Describe Get-ChordToKeystrokesHashtable  {
+
+}
+Describe Write-TopOfGrids {
+    It "Writes two grid tops and a newline when passed 2" {
+        Mock Write-Host {} -ModuleName TwiddlerLayoutPrinter
+        Write-TopOfGrids -GridsToDisplay 2
+        Assert-MockCalled Write-Host -Exactly 2 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq "┌─────────────┐" -and $NoNewLine
+        } 
+        Assert-MockCalled Write-Host -Exactly 1 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+    }
+}
+
+Describe Write-BottomOfGrids {
+    It "Writes two grid tops and a newline when passed 2" {
+        Mock Write-Host {} -ModuleName TwiddlerLayoutPrinter
+        Write-BottomOfGrids -GridsToDisplay 2
+        Assert-MockCalled Write-Host -Exactly 2 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq "└─────────────┘" -and $NoNewLine
+        } 
+        Assert-MockCalled Write-Host -Exactly 1 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+    }
+}
+
+Describe Write-ChordName  {
+    It "Writes two chord names in gray surrounded by pipes when passed 2" {
+        Mock Write-Host {} -ModuleName TwiddlerLayoutPrinter
+        Write-ChordName -GridsToDisplay 2 -ChordsToDisplay "OOOO", "LLLL"
+        Assert-MockCalled Write-Host -Exactly 2 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -match "    (OOOO|LLLL)     " -and $NoNewLine -and $ForegroundColor -eq "DarkGray"
+        } 
+        Assert-MockCalled Write-Host -Exactly 4 -ModuleName TwiddlerLayoutPrinter -ParameterFilter {
+            $Object -eq "│" -and $NoNewLine
+        }
+        Assert-MockCalled Write-Host -Exactly 1 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+    }
+}
+Describe Write-FormattedRow  {
+    It "Calls Write-FormattedRowContents with appropriate parameters" {
+        $ChordToKeystrokesParam = @{"LLLO" = "the"; "MRMM" = "5"; "MOLO" = "ti"}
+        $FormattedRowParams = @{
+            GridsToDisplay    = 2
+            ChordsToDisplay   = "OOOO", "OOMO"
+            ChordToKeystrokes = $ChordToKeystrokesParam
+            GridIndex         = 0
+            RowIndex          = 1
+        }
+        Mock Write-Host {} -ModuleName TwiddlerLayoutPrinter
+        Mock Write-FormattedRowContents {} -ModuleName TwiddlerLayoutPrinter
+        Write-FormattedRow @FormattedRowParams
+        Assert-MockCalled Write-FormattedRowContents -Exactly 2 -ModuleName TwiddlerLayoutPrinter -ParameterFilter {
+            $Row -eq 1 -and 
+            $ChordToKeystrokes -eq $ChordToKeystrokesParam -and
+            $PartialChord -match "(OOOO|OOMO)"
+        }
+    }
+}
+
+Describe Format-Layout {
+    Mock Write-Host {} -ModuleName TwiddlerLayoutPrinter
+    It "Formats Layouts" {
+
+        @"
+        "Chord","Key Strokes"
+        "   N OOOL","<Left GUI>s</Left GUI>"
+        "   O ORMM","``"
+        "   O OLMM","\"
+        "   O OMMM",";"
+        "   N OROO","<Left GUI>r</Left GUI>"
+        "   O ROOO","o"
+        "   O MOOO","i"
+        "   O LOOO","e"
+        "   O OROO","r"
+        "   O RROO","or"
+"@ | Out-File TestDrive:\Layout.csv
+        Mock Write-TopOfGrids {} -ModuleName TwiddlerLayoutPrinter
+        Mock Write-ChordName {} -ModuleName TwiddlerLayoutPrinter
+        Mock Write-FormattedRow {} -ModuleName TwiddlerLayoutPrinter
+        Mock Write-BottomOfGrids {} -ModuleName TwiddlerLayoutPrinter
+
+        Format-Layout (Get-LayoutFromCSV TestDrive:\Layout.csv) -GridsPerRow 10
+        Assert-MockCalled Write-TopOfGrids -Exactly 18 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+
+        Assert-MockCalled Write-FormattedRow -Exactly 72 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+
+        Assert-MockCalled Write-BottomOfGrids -Exactly 18 -ModuleName TwiddlerLayoutPrinter -ParameterFilter { 
+            $Object -eq $null -and -not $NoNewLine 
+        }
+    }
+}
